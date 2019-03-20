87f9aaf392d212eedf85df50d68fe009d1cc0cb7
Luka S <sensanati@gmail.com>
2019-03-20 17:12:37 +0800

    Final Commit
    Finished script and everything else

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function(){
     blackOut();
 });
 
-
 /**
  * Draw a Grid with gridSize number of Rows and Columns.
  * @param {number} gridSize How many Rows and Columns the drawing tablet will have
@@ -26,13 +25,93 @@ function drawGrid(gridSize, gridTotal) {
 }
 
 /**
+ * Removes each tabletCanvas div on the screen so that it can be reset to a blank canvas state.
+ */
+function removeGrid() {
+    var gridBox = document.querySelectorAll(".tabletCanvas");
+    gridBox.forEach((div) => {
+        div.parentNode.removeChild(div);
+    });
+}
+
+/**
  * On mouse hover over an element inside the canvas, set its background color to black.
  */
 function blackOut() {
     let drawing = document.querySelectorAll(".tabletCanvas");
     drawing.forEach((div) => {
         div.addEventListener('mouseover', function(e) {
-            e.target.setAttribute('style', 'background-color:black;');
+            e.target.setAttribute('style', 'background-color: black;');
         });
     });
 }
+
+/**
+ * On mouse hover over an element inside the canvas, set its background color to a random RGB value.
+ */
+function rainbowOut() {
+    let drawing = document.querySelectorAll(".tabletCanvas");
+    drawing.forEach((div) => {
+        div.addEventListener('mouseover', function(e) {
+            e.target.setAttribute('style', `background-color: ${randomColors()};`);
+        });
+    });
+}
+
+/**
+ * On mouse hover over an element inside the canvas, set its background color to white.
+ */
+function eraser() {
+    let drawing = document.querySelectorAll(".tabletCanvas");
+    drawing.forEach((div) => {
+        div.addEventListener('mouseover', function(e) {
+            e.target.setAttribute('style', 'background-color: white;');
+        })
+    })
+}
+
+/**
+ * Return a random combination of RGB color values.
+ */
+function randomColors() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return(`rgb( ${red}, ${green}, ${blue});`);
+}
+
+// Buttons //
+
+/**
+ * Clear the drawing field when button is clicked and prompt the user for the size of the next canvas
+ */
+var clearButton = document.querySelector('#clearButton'); /** Set event listener for the reset button */
+ clearButton.addEventListener('click', function() {
+     var gridPrompt = prompt('How large would you like your new grid to be? One side is enough');
+     if (gridPrompt === null) {return;}
+     var gridTotal = gridPrompt * gridPrompt;
+     removeGrid();
+     drawGrid(gridPrompt, gridTotal);
+     blackOut();
+ });
+
+ /**
+  * On button press, change the hover color from black to random RGB values.
+  */
+var rainbowButton = document.querySelector('#rainbowButton');
+ rainbowButton.addEventListener('click', function() {
+     rainbowOut();
+ });
+
+/**
+ * On button press, change the hover color from whatever it is currently, back to black.
+ */
+var blackButton = document.querySelector('#blackButton');
+ blackButton.addEventListener('click', function() {
+     blackOut();
+ });
+
+var eraseButton = document.querySelector('#eraseButton');
+ eraseButton.addEventListener('click', function() {
+     eraser();
+ })
